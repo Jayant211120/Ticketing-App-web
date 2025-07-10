e1bff824f8ae851d3d9206416fc9c9ee3ba8b561
Jayant <jayant62644@gmail.com>
2025-07-11 01:01:52 +0530

    fix:changes in files

--- controller/ticketOperations/student/fetch.js
+++ controller/ticketOperations/student/fetch.js
@@ -1,16 +1,26 @@
+const ticktingModel = require("../../../models/operations/generateTicket");
 
-const ticktingModel=require("../../../models/operations/generateTicket");
+const fetchData = async (req, res) => {
+  try {
+    const { role, _id } = req.user;
 
-const fetchData=async(req,res)=>{
-  try{
-    const {role}=req.user;
-    const data=await ticktingModel.find({role}).sort({_id:-1});
-    if(!data || data.length==0){
-      return res.json({status:false,message:"Data not found"});
+    let data;
+    if (role === "admin") {
+      // Admin can view all tickets
+      data = await ticktingModel.find({}).sort({ _id: -1 });
+    } else {
+      // Other users can only view their own tickets
+      data = await ticktingModel.find({ user: _id }).sort({ _id: -1 });
     }
-    res.status(200).json({status:true,message:"Data Fetch Successfully",data});
-  }catch(err){
-    res.status(400).json({status:false,message:"Something went wrong"});
+
+    if (!data || data.length === 0) {
+      return res.json({ status: false, message: "Data not found" });
+    }
+
+    res.status(200).json({ status: true, message: "Data Fetch Successfully", data });
+  } catch (err) {
+    res.status(400).json({ status: false, message: "Something went wrong" });
   }
-}
-module.exports=fetchData;
+};
+
+module.exports = fetchData;
